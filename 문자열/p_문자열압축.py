9eb5f09c16fbe0c6dbe2595cb1245b749d725ff3
jeonjihyeong <102641699+jeonjihyeong@users.noreply.github.com>
2022-07-12 20:32:20 +0900

    srting 처리 압축 구현

---
+++ 문자열/p_문자열압축.py
@@ -0,0 +1,33 @@
+s = "aabbaccc"
+
+def solution(s):
+    answer = len(s)
+    string_zip=[]
+    for i in range(1,(len(s)//2)+1):
+        a= ''
+        cnt=1
+        tmp=s[:i]
+        for j in range(i,len(s),i):
+            if s[j:j+i]==tmp:
+                cnt+=1
+            else:
+                if cnt==1:
+                    a+=tmp
+                else: a+=str(cnt)+tmp
+                tmp=s[j:j+i]
+                cnt=1
+        if cnt==1:
+            a+=tmp
+        else: a+=str(cnt)+tmp
+        string_zip.append(a)
+    
+    result= min(string_zip)
+    print(len(result))
+
+
+    
+
+    return result
+
+solution(s)
+
