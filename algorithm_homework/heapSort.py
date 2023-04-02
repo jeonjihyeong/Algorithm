e30b59504f921db170b6866a72be7e16a72c7225
jeonjihyeong <spdlqj7014@naver.com>
2023-04-02 20:38:13 +0900

    알고리즘 수업 과제

---
+++ algorithm_homework/heapSort.py
@@ -0,0 +1,29 @@
+import module
+
+def heapify(li, idx, n):
+    l = idx * 2
+    r = idx * 2+1
+    s_idx = idx
+    if(l<= n and li[s_idx] >li[l]):
+        s_idx = l
+    if(r<=n and li[s_idx] > li[r]):
+        s_idx = r
+    if s_idx != idx:
+        li[idx], li[s_idx] = li[s_idx], li[idx]
+        return heapify(li, s_idx, n)
+
+def heap_sort(v):
+    n = len(v)
+    v = [0]+v
+    for i in range(n,0,-1):
+        heapify(v,i,n)
+    
+    for i in range(n, 0, -1):
+        print(v[1])
+        v[i], v[1] = v[1], v[i]
+        heapify(v, 1, i-1)
+
+randomNumber=module.randomNumberGenerator(20)
+print(randomNumber)
+print(module.swapNumber(1,3))
+heap_sort(randomNumber)
